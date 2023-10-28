23358020f5007481d6384688baa72e3cf64d370f
jorge <jguidomingues@gmail.com>
2023-10-28 12:25:50 +0100

    new validation updates

--- spacecheck.sh
+++ spacecheck.sh
@@ -1,47 +1,51 @@
 #!/bin/bash
-source ./validation/spacecheck_validation.sh
 
 # Error message: stdout -> stderr
 usage() { echo "Usage: $0 [-d <date>] [-s <size>] [-l <limit>] [-r | -a] [-n <regex>] [<directory>]" 1>&2; exit 1; }
-argError() { echo "ERROR: $1 arg is invalid" 1>&2; exit 1; }
+argError() { echo "ERROR: $1 arg is invalid!" 1>&2; exit 1; }
+nothingFound() { echo "ERROR: nothing was found!" 1>&2; exit 1; }
 
 # --------------------------------------
 # Defaults
 # --------------------------------------
+header="$*"
+
 directory="."
 sort_option="-k1,1nr" # default sort
 size=0
-name_exp=".*"
+name_exp=".*" 
 date_ref="0000-01-01 00:00:00"
 # --------------------------------------
 # --------------------------------------
 
 
 # getopts to parse command-line options
-while getopts "d:s:l:ran::" opt; do
+while getopts "d:s:l:ran::" opt 2>/dev/null; do
   case "$opt" in
     d)
       # option -d active
-      if [[ "$OPTARG" == "" || -z "$OPTARG" ]]; then
-        argError "-d"
-      else
-        date_ref=$(LC_TIME=en_US.utf8 date -d "$OPTARG" "+%Y-%m-%d %H:%M:%S")
-      fi
+      # -d "    " - print the current date
+      date_ref=$(LC_TIME=en_US.utf8 date -d "$OPTARG" "+%Y-%m-%d %H:%M:%S" 2>/dev/null)
+      [[ $? -ne 0 ]] || [[ -z "$OPTARG" ]] && argError "-d"
+      # se for 0 ou se for nulo dá o tempo
       ;;
     s)
       # option -s active
-      if [[ "$OPTARG" =~ ^[0-9]+$ ]]; then
+      [ -z "$OPTARG" ] && argError "-s"
+      if [[ "$OPTARG" -ge 0 ]]; then
         size="$OPTARG"
+        [[ $size =~ ^[0-9]+$ ]] || argError "-s"
       else
         argError "-s"
       fi
       ;;
     l)
       # option -l active
-      if [[ "$OPTARG" =~ ^[0-9]+$ ]]; then
+      [ -z "$OPTARG" ] && argError "-l"
+      if [[ "$OPTARG" -gt 0 ]]; then
         limit_lines="$OPTARG"
       else
-        usage
+        argError "-l"
       fi
       ;;
     r)
@@ -74,7 +78,7 @@ shift $((OPTIND-1))
 
 if [[ $# -eq 1 ]]
 then
-  [ -d "$1" ] || usage  # check: valid directory
+  [ -d "$1" ] || nothingFound  # check: valid directory
   directory=$1
 fi
 
@@ -93,14 +97,14 @@ folders=$( find "$directory" -type d 2>/dev/null -exec sh -c '
   done
 ' sh {} +)
 
-[ -z "$folders" ] && usage
+[ -z "$folders" ] && nothingFound
 
 if [ -z "$limit_lines" ]; then
   limit_lines=$(echo "$folders" | wc -l)
 fi
 
 # header
-echo "SIZE NAME $(date "+%Y%m%d") $*"
+echo "SIZE NAME $(date "+%Y%m%d") $header"
 
 while IFS= read -r f; do
 
