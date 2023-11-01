cd40de5e7460cee4e7b9a49843b60d494ce81f62
Pedro Pinto <pmap@ua.pt>
2023-11-01 21:06:13 +0000

    Convert 2 array to not cumulative sum arrays

--- spacerate.sh
+++ spacerate.sh
@@ -5,30 +5,51 @@
 # -------------------------------
 new_file="$1"
 old_file="$2"
-
-sort_option="-k1,1nr"
 # -------------------------------
 # -------------------------------
 
 # header
 #echo "SIZE NAME"
 
+# Save content of new file
 declare -A new_array
 while read -r size path; do
   new_array["$path"]=$size
-done <<< "$(awk '{ print $1, $2; }' "$new_file")"
+  #echo "$path ${new_array["$path"]}"
 
+done <<< "$(tail -n +2 "$new_file" | awk '{ print $1, $2; }')"
+
+# Save content of old file
 declare -A old_array
 while read -r size path; do
   old_array["$path"]=$size
-done <<< "$(awk '{ print $1, $2; }' "$old_file")"
+done <<< "$(tail -n +2 "$old_file" | awk '{ print $1, $2; }')"
+
+# Convert new array to not cumulative sum
+while read -r size path; do
+  father_path="$path"
+  while true; do
+    [[ "${father_path%/*}" = "${father_path##/*}" ]] && break
+    father_path="${father_path%/*}"
+    new_array["$father_path"]=$(( new_array["$father_path"] - new_array["$path"] ))
+  done
+done <<< "$(tail -n +2 "$new_file" | awk '{ print $1, $2; }' | sort "-k2,2r" )"
+
+# Convert old array to not cumulative sum
+while read -r size path; do
+  father_path="$path"
+  while true; do
+    [[ "${father_path%/*}" = "${father_path##/*}" ]] && break
+    father_path="${father_path%/*}"
+    old_array["$father_path"]=$(( old_array["$father_path"] - old_array["$path"] ))
+  done
+done <<< "$(tail -n +2 "$old_file" | awk '{ print $1, $2; }' | sort "-k2,2r" )"
 
 
+for path in "${!new_array[@]}"; do
+  echo "NEW: ${new_array["$path"]} $path"
+done
 
-#
-#while IFS= read -r new_line;
-#do
-#  echo "$new_line"
-#done < "$new_file"
-
-# o size está no ${partsNewDirectory[-1]}
+for path in "${!new_array[@]}"; do
+  echo "OLD: ${new_array["$path"]} $path"
+done
