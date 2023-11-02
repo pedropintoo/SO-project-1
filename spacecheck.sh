5b65217abded73b7ad4f9f829e1aba1816ecb344
Pedro Pinto <pmap@ua.pt>
2023-11-02 23:22:28 +0000

    Fix bug

--- spacecheck.sh
+++ spacecheck.sh
@@ -96,7 +96,7 @@ then
 fi
 
 # all folders in directory
-folders=$(find "${directories[@]}" -type d | sort -u)
+folders=$(find "${directories[@]}" -type d 2>/dev/null | sort -u)
 
 if [[ -z "$limit_lines" ]]; then
   limit_lines=$(echo "$folders" | wc -l)
@@ -107,7 +107,7 @@ echo "SIZE NAME $(LC_TIME=en_US.utf8 date "+%Y%m%d") $header"
 
 while IFS= read -r f; do
 
-  if [[ -x "$f" ]]; then
+  if [[ -x "$f" ]] && [[ -r "$f" ]]; then
     bytes=$(find "$f" -not -newermt "$date_ref" -type f \( -regex "$name_exp" -o -name "$name_exp" \) -not -size -"$size"c  -exec du -bc {} + 2>/dev/null | tail -n 1 | awk '{print $1}')
     [[ -z "$bytes" ]] && bytes=0
     echo "$bytes" "$f"
